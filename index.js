39883ea1927594d5e45d23d1a0becae865c61577
Francois Laubscher <francoislaubscher@outlook.com>
2017-02-14 11:17:14 +0200

    es6 refactoring

--- index.js
+++ index.js
@@ -1,64 +1,65 @@
-const nodeZip = require('node-zip');
-const fs = require('fs');
-const Sheet = require('./sheet');
-const constants = require('./constants');
+const nodeZip = require('node-zip')
+const fs = require('fs')
+const Sheet = require('./sheet')
+const { 
+  sheetsFront,
+  sheetsBack,
+  contentTypeFront,
+  contentTypeBack,
+  relationshipFront,
+  relationshipBack,
+  relationships,
+  xlsTemplate,
+  sheet,
+  contentTypeOverride,
+  sheetRelationship
+} = require('./constants')
 
 // generate workbook
 function generateWorkbook (configs) {
-  const workbook = configs.map(function(config, i) {
-    const index = i + 1;
-    return `<sheet name="${config.name}" sheetId="${index}" r:id="rId${index}"/>`;
-  }).join('');
-
-  return `${constants.sheetsFront}${workbook}${constants.sheetsBack}`;
+  const workbook = configs.map((config, i) => sheet(config.name, i + 1)).join('')
+  return `${sheetsFront}${workbook}${sheetsBack}`
 }
 
 // generate workbook content types
-function generateContentType(configs) {
-  const contentTypes = configs.map(function (config) {
-    return `<Override PartName="/${config.fileName}" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml" />`;
-  }).join('');
-
-  return `${constants.contentTypeFront}${contentTypes}${constants.contentTypeBack}`;
+function generateContentTypes(configs) {
+  const contentTypes = configs.map((config) => contentTypeOverride(config.fileName)).join('')
+  return `${contentTypeFront}${contentTypes}${contentTypeBack}`
 }
 
-// generate workbook rels
-function generateRel(configs) {
-  const rels = configs.map(function (config, i) {
-    const index = i + 1;
-    return `<Relationship Id="rId${index}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/${config.name}.xml"/>`;
-  }).join('');
-
-  return `${constants.relFront}${rels}${constants.relBack}`;
+// generate workbook relationships
+function generateRelationships(configs) {
+  const relationships = configs.map((config, i) => sheetRelationship(config.name, i + 1)).join('')
+  return `${relationshipFront}${relationships}${relationshipBack}`
 }
 
 
-function xlr (config) {
-  const xlsx = nodeZip(constants.xlsTemplate, { base64: true, checkCRC32: false });
-  const conf = typeof config === 'object' ? [config] : config;
+function xlr (options) {
+  const xlsx = nodeZip(xlsTemplate, { base64: true, checkCRC32: false })
+  const configs = typeof options === 'object' ? [options] : options
 
   // only write the stylesheet once, so use first conf object
-  if (conf[0].stylesXmlFile) {
-    const styles = fs.readFileSync(config.stylesXmlFile, 'utf8');
+  if (configs[0].stylesXmlFile) {
+    const styles = fs.readFileSync(configs[0].stylesXmlFile, 'utf8')
     if (styles) {
-      xlsx.file('xl/styles.xml', styles);
+      xlsx.file('xl/styles.xml', styles)
     }
   }
 
   // write constants and static files
-  xlsx.file('xl/workbook.xml', generateWorkbook(conf));
-  xlsx.file('xl/_rels/workbook.xml.rels', generateRel(conf));
-  xlsx.file('_rels/.rels', constants.rels);
-  xlsx.file('[Content_Types].xml', generateContentType(conf));
+  xlsx.file('xl/workbook.xml', generateWorkbook(configs))
+  xlsx.file('xl/_rels/workbook.xml.rels', generateRelationships(configs))
+  xlsx.file('_rels/.rels', relationships)
+  xlsx.file('[Content_Types].xml', generateContentTypes(configs))
 
   // generate worksheets
-  conf.forEach(function (cfg) {
+  configs.forEach((config) => {
     // replace all characters which would cause Excel to break
-    const fileName = cfg.name ? cfg.name.replace(/[*?\]\[\/\/][ ]/g, '') : 'sheet';
-    xlsx.file(`xl/worksheets/${fileName}.xml`, Sheet(cfg));
-  });
+    const fileName = config.name ? config.name.replace(/[*?\]\[\/\/][ ]/g, '-') : 'sheet'
+    xlsx.file(`xl/worksheets/${fileName}.xml`, Sheet(config))
+  })
 
-  return xlsx.generate({ base64: false, compression: 'DEFLATE' });
+  return xlsx.generate({ base64: false, compression: 'DEFLATE' })
 }
 
-module.exports = xlr;
+module.exports = xlr
--- sheet.js
+++ sheet.js
@@ -1,62 +1,49 @@
-const constants = require('./constants');    
-const helpers = require('./helpers');
+const { sheetFront, sheetBack, columnDefinition } = require('./constants')
+const { addCell, convertToBit } = require('./helpers')
 
-function buildRow (cols, data, rowIndex) { 
-  const rows = [`<x:row r="${rowIndex}" spans="1:${cols.length}">`];
+function buildRow (columns, data, rowIndex) { 
+  const rows = [`<x:row r="${rowIndex}" spans="1:${columns.length}">`]
 
   for (var i = 0; i < data.length; i++) {
-    const cellIndex = i + 1;
+    const cellIndex = i + 1
 
-    const isObject = typeof data[i] === 'object';
-    const cellData = isObject ? data[i].value : data[i];
-    const styleIndex = isObject ? data[i].style : null;
-    const cellType = cols[i].type;
+    const isObject = typeof data[i] === 'object'
+    const cellData = isObject ? data[i].value : data[i]
+    const styleIndex = isObject ? data[i].style : null
+    const cellType = columns[i].type
+
+    const options = { 
+      cellIndex: cellIndex + rowIndex,
+      value: cellData,
+      styleIndex
+    }
 
     switch (cellType) {
     case 'number':
     case 'date':
-      rows.push(helpers.addCell({ 
-        cellIndex: cellIndex + rowIndex,
-        value: cellData,
-        styleIndex,
-        type: 'n' }));
-      break;
+      rows.push(addCell(Object.assign({ type: 'n' }, options)))
+      break
     case 'bool':
-      rows.push(helpers.addCell({ 
-        cellIndex: cellIndex + rowIndex,
-        cellData: helpers.convertToBit(cellData),
-        value: styleIndex,
-        type: 'b'
-      }));
-      break;
+      rows.push(addCell(Object.assign({ value: convertToBit(cellData), type: 'b' }, options)))
+      break
     default:
-      rows.push(helpers.addCell({ 
-        cellIndex: cellIndex + rowIndex,
-        value: cellData,
-        styleIndex
-      }));
+      rows.push(addCell(options))
     }
   }
 
-  rows.push('</x:row>');
-  return rows.join('');
+  rows.push('</x:row>')
+  return rows.join('')
 }
 
 function Sheet (config) {
-  const cols = config.cols;
-  const data = config.rows;
+  const { columns, rows } = config
 
   // build column elements
-  const colDefs = cols.map((col, i) => {
-    const index = i + 1;
-    return `<col customWidth="1" width="${col.width}" max="${index}" min="${index}"/>`;
-  }).join('');
-  
+  const columnDefinitions = columns.map((column, i) => columnDefinition(column.width, i + 1)).join('')  
   // build xml rows
-  const rows = data.map((rowData, i) => buildRow(cols, rowData, i + 1)).join('');
-
-  return `${constants.sheetFront}<cols>${colDefs}</cols><x:sheetData>${rows}</x:sheetData>${constants.sheetBack}`;
+  const xmlRows = rows.map((rowData, i) => buildRow(columns, rowData, i + 1)).join('')
+  return `${sheetFront}<cols>${columnDefinitions}</cols><x:sheetData>${xmlRows}</x:sheetData>${sheetBack}`
 }
 
-module.exports = Sheet;
+module.exports = Sheet
 
